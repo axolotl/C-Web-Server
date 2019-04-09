693bc2929097de21bde2f9c36733df2ef071cf7b
Axolotl <samuelmachat@gmail.com>
2019-04-09 19:03:31 -0400

    d20 working

--- src/server.c
+++ src/server.c
@@ -89,16 +89,14 @@ int send_response(int fd, char *header, char *content_type, void *body, int cont
 void get_d20(int fd)
 {
     // Generate a random number between 1 and 20 inclusive
+    int number = rand() % 20 + 1;
+    char *body;
+    asprintf(&body, "<h1>%d</h1>", number);
 
-    ///////////////////
-    // IMPLEMENT ME! //
-    ///////////////////
+    printf("%s\n", body);
 
     // Use send_response() to send it back as text/plain data
-
-    ///////////////////
-    // IMPLEMENT ME! //
-    ///////////////////
+    send_response(fd, "HTTP/1.1 D20", "text/html", body, strlen(body));
 }
 
 /**
@@ -168,12 +166,6 @@ void handle_http_request(int fd, struct cache *cache)
         return;
     }
 
-    ///////////////////
-    // IMPLEMENT ME! //
-    ///////////////////
-
-    // Read the three components of the first request line
-
     // If GET, handle the get endpoints
 
     //    Check if it's /d20 and handle that special case
@@ -181,20 +173,18 @@ void handle_http_request(int fd, struct cache *cache)
 
     // (Stretch) If POST, handle the post request
 
-    char *s = "GET /foobar HTTP/1.1\nHost: www.example.com\nConnection: close\n";
-
     // buffer to hold the method
     char method[200];
     // buffer to hold the path
     char path[8192];
 
-    sscanf(s, "%s %s", method, path);
+    sscanf(request, "%s %s", method, path);
 
     if (strcmp("GET", method) == 0)
     {
         if (strcmp("/d20", path) == 0)
         {
-            // do stuff
+            get_d20(fd);
         }
         else
         {
@@ -211,6 +201,7 @@ void handle_http_request(int fd, struct cache *cache)
  */
 int main(void)
 {
+    printf("made it here\n");
     int newfd;                          // listen on sock_fd, new connection on newfd
     struct sockaddr_storage their_addr; // connector's address information
     char s[INET6_ADDRSTRLEN];
@@ -234,6 +225,7 @@ int main(void)
 
     while (1)
     {
+
         socklen_t sin_size = sizeof their_addr;
 
         // Parent process will block on the accept() call until someone
